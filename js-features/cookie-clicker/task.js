57f9650854c9bf817ccd3b1c0b41d40ff7dddfe6
Mariia <nissin2008@yandex.ru>
2021-07-23 13:42:12 +0300

    Update task.js

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,7 +2,7 @@ const imageCookie = document.getElementById("cookie");
 const counter = document.getElementById("clicker__counter");
 
 function changeSize() {
-  counter.textContent = counter.textContent + 1;
+  counter.textContent = parseInt(counter.textContent) + 1;//все, вопрос снят! Я смогла задать правильный вопрос гуглу, теперь все работает
   if (counter.textContent % 2 === 0) {
     imageCookie.width = 200;
   } else {
