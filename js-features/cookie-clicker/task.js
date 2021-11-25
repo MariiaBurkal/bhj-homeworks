13a9742f83ce21138825ef080ecd689afeb71953
Mariia <79193923+MariiaBurkal@users.noreply.github.com>
2021-11-25 09:18:21 +0300

    Update task.js

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,14 +1,23 @@
-const imageCookie = document.getElementById("cookie");
-const counter = document.getElementById("clicker__counter");
+const image = document.getElementById("cookie");
+const count = document.getElementById("clicker__counter");
+const speed = document.getElementById("clicker__speed");
+let timeLastClick = 0;
 
 function changeSize() {
-  counter.textContent = parseInt(counter.textContent) + 1;//все, вопрос снят! Я смогла задать правильный вопрос гуглу, теперь все работает
-  if (counter.textContent % 2 === 0) {
-    imageCookie.width = 200;
-  } else {
-    imageCookie.width = 150;
-  }
+    count.textContent = parseInt(count.textContent) + 1;
+    if (count.textContent % 2 === 0) {
+        image.width = 200;
+    } else {
+        image.width = 150;
+    }
+
+    let nowAfterClick = new Date();
+    let timeAfterClick = nowAfterClick.getTime();
+    let t = (timeAfterClick - timeLastClick) / 1000;
+    speed.textContent = (1 / t).toFixed(2);
+
+    timeLastClick = timeAfterClick;
 }
 
-imageCookie.onclick = changeSize;
+image.onclick = changeSize;
 
