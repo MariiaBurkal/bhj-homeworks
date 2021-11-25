9788181f03c8c22c8a4d532bdfd31fa922f89fba
Mariia <79193923+MariiaBurkal@users.noreply.github.com>
2021-11-25 09:20:04 +0300

    Update task.js

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,20 +1,27 @@
-const holes = document.querySelectorAll('.hole')
-const dead = document.getElementById('dead')
-const lost = document.getElementById('lost')
+const kill = document.getElementById("dead");
+const miss = document.getElementById("lost");
 
-for (let i = 0; i<holes.length; i++) {
-    holes[i].onclick = () => {
-        if (holes[i].className.includes('hole_has-mole')) {
-            dead.textContent++;
+
+for (index = 1; index <= 9; index++) {
+    getHole = index => document.getElementById(`hole${index}`);
+    let hole = getHole(index);
+    hole.onclick = check;
+    function check() {
+        if (hole.className.includes("hole_has-mole")) {
+            kill.textContent = parseInt(kill.textContent) + 1;
+        } else {
+            miss.textContent = parseInt(miss.textContent) + 1;
         }
-        else {
-            lost.textContent++;
-        }
-        if (dead.textContent == 10) {
-            alert(`Вы победили`);
-        }
-        if (lost.textContent == 5) {
-            alert(`Вы проиграли`);
-        }
+
+        if (parseInt(kill.textContent) === 10) {
+            alert("Победа!");
+            kill.textContent = 0;
+            miss.textContent = 0;
+        } 
+        if (parseInt(miss.textContent) === 5) {
+            alert("Вы проиграли!");
+            kill.textContent = 0;
+            miss.textContent = 0;
+        } 
     }
-}
+} 
