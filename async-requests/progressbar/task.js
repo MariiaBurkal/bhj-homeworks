30bb043d63877576cc959b712fdb530c737ff5d9
Mariia <79193923+MariiaBurkal@users.noreply.github.com>
2021-11-29 19:56:06 +0300

    Update task.js

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,24 @@
+const progress = document.getElementById('progress');
+const sendingForm = document.getElementById('form');
+const url = form.getAttribute('action');
+console.log(url);
+
+sendingForm.addEventListener('submit', function () {
+    let formData = new FormData(sendingForm);
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', url);
+    
+    xhr.upload.onloadstart = function(event) {
+        progress.value = 0.5;;
+    };
+    xhr.upload.onprogress = function(event) {
+        progress.value = 0.7;;
+    };
+    xhr.upload.onload = function(event) {
+        progress.value = 1;;
+    };
+
+    xhr.send(formData);
+    event.preventDefault();
+    
+})
